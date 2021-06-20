7f6dd7f7ab4bb4dfab8af0cf489eefaa534eea01
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-20 16:57:03 +0300

    correcting syntax error

--- server/utils/orders-utils.js
+++ server/utils/orders-utils.js
@@ -1,7 +1,6 @@
 const { Orders } = require('../models');
 
 const getOrderHistory = async (req, res) => {
-    console.log('dickFace');
     const limit = +req.query.h; // turn to Number
     const orderHistory = await Orders.findAll({
         limit,
