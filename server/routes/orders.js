b1e91c000963a8d2f01eb764571a7ae0260d5f42
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-21 16:03:05 +0300

    Remove unnecessary functions

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,39 +1,6 @@
 const { Router } = require("express");
 const orders = Router();
-const models = require("../models");
-const { getOrderHistory } = require("../utils/orders-utils");
-const mongoose = require("mongoose");
-
-// routeOrders.get("/:restaurantName", async (req, res) => {
-//   const { restaurantName } = req.params;
-//   const ordersTable = await models.Orders.findAll({
-//     where: { restaurant_name: restaurantName },
-//   });
-//   const restaurantOrders = ordersTable.map((order) => {
-//     return {
-//       customerName: order.customer,
-//       dish: order.dish,
-//       drink: order.drink,
-//       restaurantName: order.restaurant_name,
-//       orderAt: new Date(order.createdAt).toLocaleString(),
-//     };
-//   });
-//   res.send(restaurantOrders);
-// });
-
-// routeOrders.post("/", async (req, res) => {
-//   const { body } = req;
-//   const orderToSave = {
-//     customer: body.customer,
-//     dish: body.dish,
-//     drink: body.drink,
-//     restaurant_name: body.restaurantName,
-//   };
-//   models.Orders.create(orderToSave);
-//   res.send("posted");
-// });
-
-// routeOrders.get('/', getOrderHistory);
+const { getOrderHistory, getDone } = require("../utils/orders-utils");
 
 const OrderModel = require("../mongoModel/mongoModel");
 
@@ -57,18 +24,20 @@ orders.post("/:restaurantName", async (req, res) => {
     restaurantName: body.restaurantName,
     _id: body.id,
   });
-
+  
   newOrder.save().then((data, err) => {
     if (!err) {
       res.send(
         data.customerName + "s order accepted! order ID: " + counter + 1
-      );
-    } else {
-      console.log(err);
-    }
+        );
+      } else {
+        console.log(err);
+      }
+    });
   });
-});
-
-// orders.patch(:/)
-
-module.exports = orders;
+  
+  // orders.patch(:/)
+  orders.get('/', getOrderHistory);
+  orders.get('/done', getDone);
+  
+  module.exports = orders;
