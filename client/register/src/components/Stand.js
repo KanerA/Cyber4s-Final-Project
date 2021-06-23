2b789b76548e4180c097a57b7dd78de811f6f6b7
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-06-23 18:30:18 +0300

    Update Stand.js

--- client/register/src/components/Stand.js
+++ client/register/src/components/Stand.js
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Stand({ stand, setRedirect, setRestaurant, user, deleteStand }) {
-  // const [standsCount, setStandsCount] = useState(stand.length);
-  // const deleteStand = async () => {
-  //   axios.delete(`stands/remove?o=${user.uid}&n=${stand.name}`);
-  //   setStandsCount(stand.length);
-  // };
   return (
     <div>
       <p
@@ -18,7 +13,6 @@ function Stand({ stand, setRedirect, setRestaurant, user, deleteStand }) {
         {stand.name}
       </p>
       <button onClick={() => deleteStand(stand.name)}> delete stand</button>
-      {/* <p>stands count: {standsCount}</p> */}
     </div>
   );
 }
