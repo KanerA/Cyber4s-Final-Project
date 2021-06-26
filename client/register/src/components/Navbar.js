cb2e9c119b258b41e9d574f1544712973c379018
Yuval Nakav <yuvalnakav1@gmail.com>
2021-06-26 22:16:22 +0300

    Update Navbar.js

--- client/register/src/components/Navbar.js
+++ client/register/src/components/Navbar.js
@@ -4,49 +4,47 @@ import { useDispatch } from "react-redux";
 import { changeRestaurant } from "../action";
 import firebase from "firebase";
 import "./styles/NavBar/NavBar.css";
-function Navbar({ restaurant, setRestaurant }) {
+function Navbar({ restaurant, setRestaurant, user }) {
   const dispatch = useDispatch();
-  function Navbar({ restaurant, setRestaurant, user }) {
-    return (
-      <div>
-        <nav>
-          <span id="restaurant-name">
-            {restaurant ? restaurant : "no restaurant"}
-          </span>
-          <span id="links">
-            <Link className="nav-link" to="/">
-              Home Page
-            </Link>
-            <Link className="nav-link" to="/menu">
-              Menu
-            </Link>
-            <Link className="nav-link" to="/create">
-              create menu
-            </Link>
-            <Link className="nav-link" to="/orders">
-              order handler
-            </Link>
-          </span>
-          <span id="buttons">
-            <button className="link-button" onClick={() => setRestaurant()}>
-              log out of stand
+  return (
+    <div>
+      <nav>
+        <span id="restaurant-name">
+          {restaurant ? restaurant : "no restaurant"}
+        </span>
+        <span id="links">
+          <Link className="nav-link" to="/">
+            Home Page
+          </Link>
+          <Link className="nav-link" to="/menu">
+            Menu
+          </Link>
+          <Link className="nav-link" to="/create">
+            create menu
+          </Link>
+          <Link className="nav-link" to="/orders">
+            order handler
+          </Link>
+        </span>
+        <span id="buttons">
+          <button className="link-button" onClick={() => setRestaurant()}>
+            log out of stand
+          </button>
+          {user && (
+            <button
+              className="link-button"
+              onClick={() => {
+                setRestaurant();
+                firebase.auth().signOut();
+              }}
+            >
+              Sign out of google
             </button>
-            {user && (
-              <button
-                className="link-button"
-                onClick={() => {
-                  setRestaurant();
-                  firebase.auth().signOut();
-                }}
-              >
-                Sign out of google
-              </button>
-            )}
-          </span>
-        </nav>
-      </div>
-    );
-  }
+          )}
+        </span>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
