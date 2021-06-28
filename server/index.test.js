acd3dbabbdf9d0e7ca107fdd67466ac2ecf6a857
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-28 20:49:38 +0300

    Add tests for orders
    Test get all restaurant orders and create new order

--- server/index.test.js
+++ server/index.test.js
@@ -19,3 +19,16 @@ const newOrder = {
     restaurantName: 'Assaf\'s pizza',
     totalPrice: 170,
   };
+
+describe('Test orders routes', () => {
+    it('Should GET all orders by the restaurant\'s name', async () => {
+        const res = await request(app).get('/orders/Assaf\'s pizza');
+        expect(res.status).toBe(200);
+    });
+
+    test('Create new order', async () => {
+        const res = await request(app).post('/orders/Assaf\'s pizza').send(newOrder)
+        expect(res.status).toBe(200);
+        expect(res.text).toBe('Assafs order accepted!');
+    });
+});
