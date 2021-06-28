7e40aa97cb908b72ab0062e3f2faa5f8a6c8e8a3
Yuval Nakav <yuvalnakav1@gmail.com>
2021-06-28 18:04:30 +0300

    added KOgin and Orders views and configuration for  firebase in native app

---
+++ client/cook/views/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, SafeAreaView, Text } from "react-native";
+import auth from "../firebaseConfig";
+
+export default function Login() {
+  return (
+    <SafeAreaView>
+      <Text></Text>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({});
