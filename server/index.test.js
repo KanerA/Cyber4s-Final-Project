edc0bb94f25cfb18e84c783a0140a808691d9016
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-30 18:52:11 +0300

    add seed array for test mongo

--- server/index.test.js
+++ server/index.test.js
@@ -1,5 +1,180 @@
 const request = require('supertest');
+const OrderModel = require('./mongoModel/testMongoModel');
 const app = require('./app');
+
+const seedArr = [
+    {
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'water',
+            price: 1,
+            note: 'none',
+            amount: 3,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'David',
+        dish: [{
+            name: 'Hamburger',
+            price: 10,
+            notes: 'Very good hamburger',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Spaghetti',
+            price: 15,
+            notes: 'Very good spaghetti',
+            amount: 1,
+          }],
+        drink: [{
+            name: 'Cola',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 150,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    },{
+        customerName: 'Assaf',
+        dish: [{
+            name: 'Pizza',
+            price: 10,
+            notes: 'Very good pizza',
+            amount: 2,
+          }],
+        drink: [{
+            name: 'Pizza',
+            price: 10,
+            note: 'none',
+            amount: 15,
+          }],
+        createdAt: Date.now(),
+        restaurantName: 'Assaf\'s pizza',
+        totalPrice: 170,
+    }
+]
 
 const newOrder = {
     customerName: 'Assaf',
@@ -18,11 +193,11 @@ const newOrder = {
     createdAt: Date.now(),
     restaurantName: 'Assaf\'s pizza',
     totalPrice: 170,
-  };
+};
 
-  const mockParams = {
-      h: 10, // for get history test, change for other history length
-  }
+const mockParams = {
+    h: 10, // for get history test, change for other history length
+};
 
 describe('Test orders routes', () => {
     it('Should GET all orders by the restaurant\'s name', async () => {
@@ -33,7 +208,7 @@ describe('Test orders routes', () => {
     test('Create new order', async () => {
         const res = await request(app).post('/orders/Assaf\'s pizza').send(newOrder)
         expect(res.status).toBe(200);
-        expect(res.text).toBe('Assafs order accepted!');
+        expect(res.text).toBe('Assaf\'s order accepted!');
     });
 
     test('Getting order history', async () => {
