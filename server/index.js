4e551b7460ee81e6aa9741ada8e5c5228d7275c6
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-30 18:36:43 +0300

    Remove mongoose connection

--- server/index.js
+++ server/index.js
@@ -3,22 +3,8 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const env = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8080;
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log(`connected to MongoDB`);
-    app.listen(PORT, () => {
-        console.log(`app is listening on port - ${PORT}`);
-        console.log(`work environment - ${env}`);
-    });
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
+        
+app.listen(PORT, () => {
+    console.log(`app is listening on port - ${PORT}`);
+    console.log(`work environment - ${env}`);
+});
