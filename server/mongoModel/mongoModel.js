fe6b8e1a202bcdc8415a7626725fb6d2facb1378
Assaf Kaner <kaner.assaf@gmail.com>
2021-06-30 15:03:25 +0300

    Move mongo connection to index.js

--- server/mongoModel/mongoModel.js
+++ server/mongoModel/mongoModel.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log(`connected to MongoDB`);
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const OrderSchema = new mongoose.Schema({
   customerName: String,
   dish: [{ type: Object }],
