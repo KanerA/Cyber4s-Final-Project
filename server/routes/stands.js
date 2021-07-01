25420b90619181eaa7aacb4d81208cea2594e24c
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-01 14:19:45 +0300

    change create stand route

--- server/routes/stands.js
+++ server/routes/stands.js
@@ -4,7 +4,7 @@ const stands = Router();
 const { createNewStand, getAllStands, deleteStand } = require("../utils/stands-utils");
 
 stands.get("/:uid", getAllStands);
-stands.post("/", createNewStand);
-stands.delete('/remove', deleteStand)
+stands.post("/create", createNewStand);
+stands.delete('/remove', deleteStand);
 
 module.exports = stands;
