12aba594f4f0e99ae49fbac3f077f5618b8f3f4b
Yuval Nakav <yuvalnakav1@gmail.com>
2021-07-01 16:20:37 +0300

    moved passwordRef to standCreator, edited login function

--- client/cook/screens/Login.js
+++ client/cook/screens/Login.js
@@ -14,10 +14,33 @@ import axios from "axios";
 export default function Login({ setRestaurant }) {
   const standNameInput = useRef();
   const passwordInput = useRef();
+  // const Proxy =
 
-  const logIntoStandOrders = () => {
+  const logIntoStandOrders = async () => {
     // check the password and stand name input in the sql database
     // axios.get(`http://10.0.0.5:8080/stands/${standNameInput.current}`); // NEED TO CREATE ROUTE IN SERVER
+    // try {
+    //   const res = await axios.post(
+    //     `${Proxy}/stands/login/${username}`,
+    //     {
+    //       name: username.current,
+    //       password: passwordRef.current,
+    //     },
+    //     {
+    //       headers: {
+    //         authorization: "Bearer " + localStorage.accessToken,
+    //       },
+    //     }
+    //   );
+    //   // if (res.status === 201) return setLoginError(true);
+    //   localStorage.setItem("userId", res.data.id);
+    //   localStorage.setItem("accessToken", res.data.accessToken);
+    //   localStorage.setItem("refreshToken", res.data.refreshToken);
+    //   document.location.pathname = "/trivia";
+    // } catch (err) {
+    //   console.log(err);
+    //   // setLoginError(true);
+    // }
     const response = "b"; // the axios response (standNameInput)
     setRestaurant(response);
   };
--- client/register/src/components/Login.js
+++ client/register/src/components/Login.js
@@ -1,16 +1,15 @@
 import React from "react";
-
 import firebase from "firebase";
 
 function Login() {
   const signUpWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-
     firebase.auth().signInWithPopup(provider);
   };
   return (
     <div>
       <button
+        id="google-login"
         onClick={() => {
           signUpWithGoogle();
         }}
