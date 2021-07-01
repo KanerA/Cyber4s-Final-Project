7d6f0649dc143d67196297fef43d925466ff1919
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-01 16:53:07 +0300

    add login function to stands route

--- server/routes/stands.js
+++ server/routes/stands.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const stands = Router();
 
-const { createNewStand, getAllStands, deleteStand } = require("../utils/stands-utils");
+const { createNewStand, getAllStands, deleteStand, standLogin } = require("../utils/stands-utils");
 
 stands.get("/:uid", getAllStands);
 stands.post("/create", createNewStand);
 stands.delete('/remove', deleteStand);
+stands.post('/login', standLogin);
 
 module.exports = stands;
