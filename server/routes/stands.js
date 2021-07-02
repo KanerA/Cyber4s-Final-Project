916bdb2657293f7b3c3b9c37ef6b61a434355353
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-02 15:57:18 +0300

    add PW middleware to routes
    PW = password...

--- server/routes/stands.js
+++ server/routes/stands.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
+const { validatePassword } = require("../middlewares");
 const stands = Router();
 
 const { createNewStand, getAllStands, deleteStand, standLogin } = require("../utils/stands-utils");
 
 stands.get("/:uid", getAllStands);
-stands.post("/create", createNewStand);
-stands.delete('/remove', deleteStand);
-stands.post('/login', standLogin);
+stands.post("/create", validatePassword, createNewStand);
+stands.delete('/remove', validatePassword, deleteStand);
+stands.post('/login', validatePassword, standLogin);
 
 module.exports = stands;
