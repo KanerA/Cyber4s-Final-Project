c98e66917851fcc777ef73ebd27aa0478037fab2
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-02 15:56:38 +0300

    adjust the stand route's utils
    adjust the routes to the new middleware

--- server/utils/stands-utils.js
+++ server/utils/stands-utils.js
@@ -6,19 +6,13 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 const createNewStand = async (req, res) => {
-  const { body: { restaurant_name, password} } = req;
-  const isExist = await Stands.findOne({ // checking if the restaurant's name already registered in the DB
-    where: {
-      name: restaurant_name
-    }
-  })
-  if(isExist) return res.json({ message: 'Restaurant name is already registered'});
-  if(password.length < 6) return res.json({ message: 'Password is too short, please choose another'}); // validating length of password
-  const hashedPW = hashSync(password, 10); // hashing the password for DB
+  const password = req.body.password;
+    if(password.length < 6) return res.json({ message: 'Password is too short, please choose another' }); // validating length of password
+    const hashedPW = hashSync(password, 10); // hashing the password for DB
   const user_name = randomString.generate(6); // creating the restaurant's user name
   const stand = await Stands.create({
     user_name,
-    name: restaurant_name,
+    name: req.body.restaurant_name,
     password: hashedPW,
   });
 
@@ -35,28 +29,25 @@ const createNewStand = async (req, res) => {
   res.status(201).json({ accessToken, refreshToken, id, user_name });
 };
 
-const getAllStands = (req, res) => {
-  const {
-    body: { restaurant_name, password },
-  } = req;
-  Stands.findAll({
-    // where: { owner: uid },
-  }).then((result) => {
-    const allStands = result.map((stand) => stand.toJSON());
-    res.json(allStands);
-  });
+// const getAllStands = (req, res) => {
+//   const {
+//     body: { restaurant_name, password },
+//   } = req;
+//   Stands.findAll({
+//     // where: { owner: uid },
+//   }).then((result) => {
+//     const allStands = result.map((stand) => stand.toJSON());
+//     res.json(allStands);
+//   });
+// };
+
+const getAllStands = async (req, res) => { // only with JWT authentication
+  const stands = await Stands.findAll({});
+  res.json(stands);
 };
 
 const standLogin = async (req, res) => {
-  const { body: { restaurant_name, password }} = req;
-  const stand = await Stands.findOne({
-    where: {
-      name: restaurant_name,
-    }
-  });
-  if(!stand) return res.status(201).json({message: 'Restaurant doesn\'t exist, please sign up'}); // check if the stand exists
-  const isPWCorrect = await compare(password, stand.password);
-  if(!isPWCorrect) return res.sendStatus(403); // check if the password matches
+  const stand = req.stand;
   const payload = {
     name: stand.name,
     password: stand.password,
