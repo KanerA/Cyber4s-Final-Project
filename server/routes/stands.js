cf9d6c07e737a3b07d4ed06ee029455ea79ab58e
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-02 19:10:17 +0300

    add token validation to stand routes

--- server/routes/stands.js
+++ server/routes/stands.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
-const { validatePassword } = require("../middlewares");
+const { validateToken, validatePassword } = require("../middlewares");
 const stands = Router();
-
 const { createNewStand, getAllStands, deleteStand, standLogin } = require("../utils/stands-utils");
 
 stands.get("/:uid", getAllStands);
 stands.post("/create", validatePassword, createNewStand);
-stands.delete('/remove', validatePassword, deleteStand);
+stands.delete('/remove', validateToken, validatePassword, deleteStand);
 stands.post('/login', validatePassword, standLogin);
 
 module.exports = stands;
