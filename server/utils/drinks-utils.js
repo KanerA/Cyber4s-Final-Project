242ad79851f5c112094697086393a3b48040e8dc
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-04 19:42:15 +0300

    fix get all drinks

--- server/utils/drinks-utils.js
+++ server/utils/drinks-utils.js
@@ -5,26 +5,34 @@ const getAllRestaurantDrinks = (req, res) => {
   if(req.user.user_name !== user_name) return res.sendStatus(403);
   Drinks.findAll({
     where: { restaurant_name: user_name },
-  }).then((drinks) => {
-    const allDrinks = drinks.map((drink) => drink.toJSON());
-    res.send(
-      allDrinks.map((drink) => {
-        return {
-          name: drink.name,
-          description: drink.description,
-          restaurantName: drink.user_name,
-          price: drink.price,
-          alcoholic: drink.alcoholic,
-        };
-      }),
-    );
-  });
+  })
+    .then((drinks) => {
+      const allDrinks = drinks.map((drink) => drink.toJSON());
+      res.json(
+        allDrinks.map((drink) => {
+          return {
+            name: drink.name,
+            description: drink.description,
+            restaurantName: drink.user_name,
+            price: drink.price,
+            alcoholic: drink.alcoholic,
+          };
+        })
+      )
+    })
+    .catch(err => console.log(err))
 };
 
 const createNewDrink = (req, res) => {
-  const { body } = req;
-  if(req.user.user_name !== body.user_name) return res.sendStatus(403);
-  Drinks.create(body).then(() => res.send("new drink created"));
+  const { body: { name, description, user_name, price, alcoholic }} = req;
+  if(req.user.user_name !== user_name) return res.sendStatus(403);
+  Drinks.create({
+    name,
+    description,
+    restaurant_name: user_name,
+    price,
+    alcoholic,
+  }).then(() => res.send("new drink created"));
 };
 
 module.exports = { getAllRestaurantDrinks, createNewDrink };
