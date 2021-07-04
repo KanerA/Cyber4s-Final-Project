124655d56908d2733ce8aa2c7eee5c41caa699c8
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-04 18:17:36 +0300

    Update stands.js

--- server/routes/stands.js
+++ server/routes/stands.js
@@ -1,7 +1,13 @@
 const { Router } = require("express");
 const { validateToken, validatePassword } = require("../middlewares");
 const stands = Router();
-const { createNewStand, getAllStands, deleteStand, standLogin } = require("../utils/stands-utils");
+const { 
+    createNewStand,
+    getAllStands,
+    deleteStand,
+    standLogin,
+    getStandData
+} = require("../utils/stands-utils");
 
 stands.get("/:uid", getAllStands);
 stands.post("/create", validatePassword, createNewStand);
