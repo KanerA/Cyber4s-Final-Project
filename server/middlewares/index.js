4b70d67742ea33bc80110ae68b8ede1412791672
Assaf Kaner <kaner.assaf@gmail.com>
2021-07-05 16:47:32 +0300

    Update index.js

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -6,10 +6,12 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 const validateToken = (req, res, next) => {
     const bearerToken = req.headers['authorization']; // get the token from the request
+    // console.log(bearerToken);
     if(!bearerToken) return res.sendStatus(403);
     req.token = bearerToken.slice(7); // trim the 'bearer' from token
     return jwt.verify(req.token, ACCESS_TOKEN_SECRET, (err, decoded) => { // verify the token with the SECRET
         if(err){
+            if(err.message === 'jwt expired') return res.json({ message: 'Token Expired' })
             return res.status(403).json({ message: 'Invalid Access Token' });
         }
         req.user = decoded;
