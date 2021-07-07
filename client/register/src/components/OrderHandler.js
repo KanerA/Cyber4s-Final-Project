eb2838f8ef8b3391f9b7c39f2595fd8f404d3d07
Yuval Nakav <yuvalnakav1@gmail.com>
2021-07-07 22:53:09 +0300

    initial commit

--- client/register/src/components/OrderHandler.js
+++ client/register/src/components/OrderHandler.js
@@ -7,27 +7,29 @@ function OrderHandler({ restaurant, restaurantUser }) {
   const [orders, setOrders] = useState([]);
   const [canceled, setCanceled] = useState(false);
 
-  useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected");
-      socket.on("getNewOrder", (newOrder) => {
-        setOrders((prev) => [...prev, newOrder]);
-      });
+  socket.on("connect", () => {
+    console.log("connected");
+    socket.on("getNewOrder", (newOrder) => {
+      setOrders((prev) => [...prev, newOrder]);
     });
+  });
+  useEffect(() => {
     axios
       .get(`/orders/${restaurantUser}`)
       .then((res) => {
         setOrders(res.data);
       })
       .catch((err) => console.log(err));
-  }, [, orders]);
+  }, [,]);
   const cancelOrder = (order) => {
     alert(`${order.customerName}'s order is canceled!`);
-
     axios
       .patch(`/orders/done/?c=true&id=${order._id}`)
       .then((res) => setCanceled(true))
       .catch((err) => console.log(err));
+    // console.log(order);
+    const canceled = orders.filter((canceled) => canceled._id !== order._id);
+    setOrders(canceled);
   };
   return (
     <div className="orders">
